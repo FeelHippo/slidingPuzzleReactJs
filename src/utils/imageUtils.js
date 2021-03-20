f4cd252a1b3eee9792f3ff43967032f5c1189dd5
Filippo Miorin <filippomiorin@MBP-de-Mateo.home>
2021-03-20 23:49:37 +0100

    Uint8ClampedArray validation

--- src/utils/imageUtils.js
+++ src/utils/imageUtils.js
@@ -1,3 +1,6 @@
+const { PNG } = require('pngjs');
+const jsqr = require('jsqr');
+
 export const imageToBase64 = async imageFile => {
     return await new Promise(resolve => {
         const reader = new FileReader();
@@ -32,8 +35,8 @@ const sliceTiles = picture => {
             tempCanvas.height = tileHeight;
             const tempContext = tempCanvas.getContext('2d');
             if (Math.random() < 0.2 && !oneElementIsBlank) {
+                imageData = context.createImageData(tileWidth, tileHeight);
                 oneElementIsBlank = true;
-                imageData = context.createImageData(tileWidth, tileHeight)
             }
             tempContext.putImageData(imageData, 0, 0);
             tilesArray = [
@@ -45,18 +48,16 @@ const sliceTiles = picture => {
     return tilesArray;
 }
 
-const BLANK_TILE_DEFAULT = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAHcAAABCCAYAAABtjkmUAAAA9ElEQVR4Xu3TAREAAAiDQNe/tD3+sAHgdh1rYCxZYFdc+AmKW1zYAIzWcosLG4DRWm5xYQMwWsstLmwARmu5xYUNwGgtt7iwARit5RYXNgCjtdziwgZgtJZbXNgAjNZyiwsbgNFabnFhAzBayy0ubABGa7nFhQ3AaC23uLABGK3lFhc2AKO13OLCBmC0lltc2ACM1nKLCxuA0VpucWEDMFrLLS5sAEZrucWFDcBoLbe4sAEYreUWFzYAo7Xc4sIGYLSWW1zYAIzWcosLG4DRWm5xYQMwWsstLmwARmu5xYUNwGgtt7iwARit5RYXNgCjtVw47gOToQBDC7+/1AAAAABJRU5ErkJggg=='
-
 export const moveClickedTile = (currentBoard, tileClicked) => {
     // determine if clicked tile is adjacent to blank one
     const likelyValid = currentBoard.some((tile, index) => 
-        tile === BLANK_TILE_DEFAULT &&
+        transparentTile(tile) &&
         [1, -1, 4, -4].some(offset => index === tileClicked + offset)
     )
     // swap tiles
     return likelyValid ? currentBoard.map((tile, index) => {
-        if (tile === BLANK_TILE_DEFAULT) return currentBoard[tileClicked]
-        else if (index === tileClicked) return BLANK_TILE_DEFAULT
+        if (transparentTile(tile)) return currentBoard[tileClicked]
+        else if (index === tileClicked) return currentBoard.filter(tile => transparentTile(tile))[0]
         return tile
     }) : currentBoard
 }
@@ -65,3 +66,9 @@ export const compareArrays = (current, winner) => {
     return current.length === winner.length &&
     current.every((v, i) => v === winner[i])
 }
+
+const transparentTile = base64 => {
+    const tileToPNG = PNG.sync.read(Buffer.from(base64.split(',')[1], 'base64'));
+    const uint8 = Uint8ClampedArray.from(tileToPNG.data);
+    return uint8.every(bit => bit === 0)
+} 
